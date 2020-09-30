781353848b35748cec4189101d9099787cacc413
muhammadalised <muhammadalised@gmail.com>
2020-09-30 15:08:26 +0500

    added fetch and csrf token functionality for cart

--- static/js/cart.js
+++ static/js/cart.js
@@ -8,9 +8,30 @@ for(var i = 0; i < updateBtns.length; i++){
 
         console.log('User: ', user)
         if (user == 'AnonymousUser'){
-            console.log('Not logged in')
+            console.log('User is not authenticated')
         } else{
-            console.log('User is logged in, sending data...')
+            updateUserOrder(productId, action)
         }
     })
+}
+
+function updateUserOrder(productId, action){
+    console.log('User is authenticated, sending data...')
+
+    var url = '/update_item/'
+
+    fetch(url, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+            'X-CSRFToken': csrftoken,
+        },
+        body: JSON.stringify({'productId': productId, 'action': action})
+    })
+    .then((response) => {
+        return response.json();
+    })
+    .then((data) => {
+        location.reload();
+    });
 }
