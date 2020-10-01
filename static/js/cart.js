45bc3c627dfe8dadccad7476aec1755e01a6b5e3
muhammadalised <muhammadalised@gmail.com>
2020-10-01 15:48:48 +0500

    Build cart items

--- static/js/cart.js
+++ static/js/cart.js
@@ -1,69 +1,3 @@
-<<<<<<< HEAD
-var updateBtns = document.getElementsByClassName('update-cart')
-
-for(var i = 0; i < updateBtns.length; i++){
-    updateBtns[i].addEventListener('click', function(){
-        var productId = this.dataset.product
-        var action = this.dataset.action
-        console.log('Product Id: ', productId, "Action: ", action)
-
-        console.log('User: ', user)
-        if (user == 'AnonymousUser'){
-            addCookieItem(productId, action)
-        } else{
-            updateUserOrder(productId, action)
-        }
-    })
-}
-
-function updateUserOrder(productId, action){
-    console.log('User is authenticated, sending data...')
-
-    var url = '/update_item/'
-
-    fetch(url, {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json',
-            'X-CSRFToken': csrftoken,
-        },
-        body: JSON.stringify({'productId': productId, 'action': action})
-    })
-    .then((response) => {
-        return response.json();
-    })
-    .then((data) => {
-        location.reload();
-    });
-}
-
-function addCookieItem(productId, action){
-	console.log('User is not authenticated')
-
-	if (action == 'add'){
-		if (cart[productId] == undefined){
-		cart[productId] = {'quantity':1}
-
-		}else{
-			cart[productId]['quantity'] += 1
-		}
-	}
-
-	if (action == 'remove'){
-		cart[productId]['quantity'] -= 1
-
-		if (cart[productId]['quantity'] <= 0){
-			console.log('Item should be deleted')
-			delete cart[productId];
-		}
-	}
-	console.log('CART:', cart)
-	document.cookie ='cart=' + JSON.stringify(cart) + ";domain=;path=/"
-	
-	location.reload()
-}
-||||||| empty tree
-=======
 var updateBtns = document.getElementsByClassName('update-cart')
 
 for(var i = 0; i < updateBtns.length; i++){
@@ -129,4 +63,3 @@ function updateUserOrder(productId, action){
         location.reload();
     });
 }
->>>>>>> 69942c61494c239cd97ec39bd824ca8e03a9c68b
